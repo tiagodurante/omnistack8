59b804df64a72aaf2a5cd76edc247a920927d52f
Tiago Amado Durante <tiagodurante@icloud.com>
2019-08-06 15:22:09 -0300

    criando crud de devs

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const routes = require("./routes");
 const server = express();
+
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@omnistack-db-i7odu.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
+
 server.use(express.json());
 server.use(routes);
 server.listen(3333);
