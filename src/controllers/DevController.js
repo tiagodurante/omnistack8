4fa584894bd2c2c4f0c854906f48aa2984852c00
Tiago Amado Durante <tiagodurante@icloud.com>
2019-08-06 15:45:05 -0300

    dando like via api

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,5 +1,29 @@
+const axios = require("axios");
+const Dev = require("../models/Dev");
+
 module.exports = {
   async store(req, res) {
-    res.json(true);
+    const { username } = req.body;
+
+    const userExists = await Dev.findOne({
+      username
+    });
+
+    if (userExists) {
+      return res.json(userExists);
+    }
+
+    let response = await axios.get(`https://api.github.com/users/${username}`);
+
+    const { name, bio, avatar_url: avatar } = response.data;
+
+    const dev = await Dev.create({
+      name,
+      username,
+      bio,
+      avatar
+    });
+
+    return res.json(dev);
   }
 };
