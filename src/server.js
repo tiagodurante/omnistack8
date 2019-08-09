ed2f946fe6da41719fa724f4ebcd5042c8a50398
Tiago Amado Durante <tiagodurante@icloud.com>
2019-08-09 14:16:11 -0300

    implementando websocket

--- src/server.js
+++ src/server.js
@@ -2,7 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes");
-const server = express();
+const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+io.on("connection", socket => {
+  console.log("nova conexão", socket.id);
+  socket.on("hello", message => {
+    console.log(message);
+  });
+});
 
 mongoose.connect(
   "mongodb+srv://omnistack:omnistack@omnistack-db-i7odu.mongodb.net/test?retryWrites=true&w=majority",
@@ -11,7 +21,7 @@ mongoose.connect(
   }
 );
 
-server.use(express.json());
-server.use(cors());
-server.use(routes);
+app.use(express.json());
+app.use(cors());
+app.use(routes);
 server.listen(3333);
